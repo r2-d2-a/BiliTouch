4d40cce1a72b428d470f0dda437fd6abaf2d9da8
r2-d2-a <2433422790@qq.com>
2023-01-15 23:25:55 +0800

    completed version

--- source/common.js
+++ source/common.js
@@ -1,34 +1,35 @@
 window.addEventListener('load', () =>
 {
-    var video = document.querySelector('video')
-    console.log(video)
+    video = document.querySelector('video')
     document.addEventListener("touchstart", e => {
         if(e.changedTouches.length == 1){
             ;[...e.changedTouches].forEach(touch=>{
+            if(video == null)
+            {
+                 video = document.querySelector('video')
+            }
             video.setAttribute("touchX", touch.clientX)
-            console.log(video.getAttribute("touchX"))
             }) 
         }
     //第三个参数 true or false 是指事件发生之后，事件会先逐层进入各层元素，直至最底层，然后冒泡出来，事件监听器中的函数是在进去还是出来的过程中执行,false 是在冒泡中执行
     },true)
 
     document.addEventListener("touchmove", e => {
-        console.log("catch move")
-        // e.preventDefault()
-        // e.stopPropagation()
+        e.preventDefault()
+        e.stopPropagation()
         if(e.targetTouches.length == 1){
 
             ;[...e.changedTouches].forEach(touch=>{
+            if(video == null)
+            {
+                video = document.querySelector('video')
+            }
             video.setAttribute("moveX", touch.clientX)
-            console.log(video.getAttribute("moveX"))
-            console.log(video.currentTime)
-            video.currentTime += 0.1*(video.getAttribute("moveX") - video.getAttribute("touchX"))
+            video.currentTime += 0.2*(video.getAttribute("moveX") - video.getAttribute("touchX"))
             video.setAttribute("touchX", video.getAttribute("moveX"));
-            console.log(video.currentTime)
             video.play()
             } ) 
         }
     },true)
-
 })
 
